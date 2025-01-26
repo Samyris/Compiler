bde4bccdcbb4efbe1d7e0662962f6d6759ad4a93
Samyris <msamyris@gmail.com>
2025-01-25 21:47:48 -0300

    update: classe compiler _da erro na leitura de arquivo

--- src/compiler_modules/Compiler.java
+++ src/compiler_modules/Compiler.java
@@ -1,7 +1,14 @@
 package compiler_modules;
 
 import compiler_modules.lexer.*;
+import compiler_modules.parser.Parser;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import shared.errors.CompilerError;
 
 public class Compiler {
 
@@ -13,22 +20,28 @@ public class Compiler {
 
         scanner.close();
 
-        System.out.println("Processando análise léxica: ");
+        System.out.println("Processando análise léxica e sintática: ");
 
         // Processa o arquivo usando o Lexer
         try {
-            Lexer lexer = new Lexer("C:\\Users\\Samyris\\Documents\\CEFET\\Compiladores\\Compiler\\test_programs\\" + fileName);
+            String sourceCode = new String(Files.readAllBytes(Paths.get("C:\\Users\\Samyris\\Documents\\CEFET\\Compiladores\\Compiler\\test_programs\\" + fileName)));
+            Lexer lexer = new Lexer(sourceCode);
+            List<Token> tokens = new ArrayList<>();
             Token token;
 
             while ((token = lexer.scan()) != null) {
-                // System.out.println(token); // Imprime cada token
-                System.out.println(
-                        String.format("Token: <\"%s\", %s>, Tag: %s", token, token.mapTagToString(),
-                                token.getTagValueAsString()));
-
+                tokens.add(token);
             }
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
+
+            // Processa os tokens usando o Parser
+            Parser parser = new Parser(tokens);
+            parser.parse();
+
+            System.out.println("Parsing completed successfully.");
+        } catch (IOException e) {
+            System.err.println("Error reading source file: " + e.getMessage());
+        } catch (CompilerError e) {
+            System.err.println("Syntax error at line " + e.getLine() + ": " + e.getMessage());
         }
     }
 }
